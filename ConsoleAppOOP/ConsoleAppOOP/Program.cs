98da467df42c78c27b5984c05630c0fcb70d83a4
Luis Ricardo <luiscastrodev@gmail.com>
2022-04-09 22:52:27 -0300

    init project

---
+++ ConsoleAppOOP/ConsoleAppOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }     
+    }
+}
