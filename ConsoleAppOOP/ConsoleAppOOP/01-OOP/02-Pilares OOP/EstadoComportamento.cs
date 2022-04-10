1bd9d1be808ef78461e02cd024796b382c070e49
Luis Ricardo <luiscastrodev@gmail.com>
2022-04-09 23:09:00 -0300

    Estado Vs Comportamento

--- ConsoleAppOOP/ConsoleAppOOP/01-OOP/02-Pilares OOP/EstadoComportamento.cs
+++ ConsoleAppOOP/ConsoleAppOOP/01-OOP/02-Pilares OOP/EstadoComportamento.cs
@@ -9,5 +9,22 @@ namespace ConsoleAppOOP._01_OOP._02_Pilares_OOP
     public class Pessoa
     {
 
+        //Estado propriedades dessa classe, ela possui um estado , tem um estado definido
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+
+        //Transforma data de nascimento em idade , comportamento transformando informacoes e devolvendo outro
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+
+            return idade;
+        }
     }
 }
